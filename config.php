a87c1bc1602f2e61084162d0ebe9c7c5328d3e0a
Alexandre Canoën <contact@acanoen.fr>
2018-12-21 20:05:23 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+
+    // Configuration file
+    return [
+
+        // Database host
+        "host" => "127.0.0.1",
+
+        // Database name
+        "name" => "microblog",
+
+        // Database username
+        "user" => "root",
+
+        // Database password
+        "pass" => "root",
+
+        // Debug mode
+        // Show errors or not
+        "debug" => true
+
+    ];
