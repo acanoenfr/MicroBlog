9c5e122d8345fa5728605dd84585bcd6be8d862a
Alexandre Canoën <contact@acanoen.fr>
2018-12-21 20:35:12 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@
     function connectToDatabase()
     {
         try {
-            $db = new \PDO("mysql:host{$_ENV['host']};dbname={$_ENV['name']}", $_ENV['user'], $_ENV['pass']);
+            $db = new \PDO("mysql:host={$_ENV['host']};dbname={$_ENV['name']}", $_ENV['user'], $_ENV['pass']);
             $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
             $db->setAttribute(\PDO::ATTR_CASE, \PDO::CASE_LOWER);
             $db->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
